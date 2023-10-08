cde992c7fa85707ce74c9bd550babd3c6ec652f6
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-10-07 22:00:06 -0700

    Styling calibration for EVERYTHING

--- static/js/main.js
+++ static/js/main.js
@@ -6,11 +6,13 @@ toggleSwitch.addEventListener("change", () => {
     if (!toggleSwitch.checked) {
         root.style.setProperty('--background', '#FAFAFA');
         root.style.setProperty('--text', 'black');
+        root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.1)')
         logoImg.src = "/static/images/logo-light.png";
     }
     else {
         root.style.setProperty('--background', 'rgb(23,23,23)');
         root.style.setProperty('--text', 'white');
+        root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.9)')
         logoImg.src = "/static/images/logo-dark.png";
     }
 });
