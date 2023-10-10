5bef13bc8edb578dffd102496ec54370a4bf8061
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-10-09 21:58:36 -0700

    Started on comments and forms

--- app/models.py
+++ app/models.py
@@ -9,6 +9,14 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'posts'
+
     def __str__(self):
         """Return a string reprsentation of the model."""
         return f"{self.title}"
+    
+class Comment(models.Model):
+    """A comment by a user."""
+    date_added = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=200)
