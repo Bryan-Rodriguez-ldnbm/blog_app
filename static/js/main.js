2b6ba9b523054d5d300d8d0846fa72425e0de8e6
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-10-11 23:13:40 -0700

    buncha commits

--- static/js/main.js
+++ static/js/main.js
@@ -7,12 +7,10 @@ toggleSwitch.addEventListener("change", () => {
         root.style.setProperty('--background', '#FAFAFA');
         root.style.setProperty('--text', 'black');
         root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.1)')
-        logoImg.src = "/static/images/logo-light.png";
     }
     else {
         root.style.setProperty('--background', 'rgb(23,23,23)');
         root.style.setProperty('--text', 'white');
         root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.9)')
-        logoImg.src = "/static/images/logo-dark.png";
     }
 });
