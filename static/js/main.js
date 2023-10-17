fe153dcae4c3216d5579e2b63710a4f3dc5abda2
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-10-16 22:06:20 -0700

    Integrated comments and comment sessions with styling cookies and whatnot

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,43 @@
 const root = document.documentElement;
-const logoImg = document.getElementById("logo-img");
 const toggleSwitch = document.getElementById("toggleSwitch");
+
+function setMode(mode) {
+    localStorage.setItem('UserModePref', mode);
+}
+
+function getMode() {
+    const mode = localStorage.getItem('UserModePref')
+    if (mode === 'dark') {
+        setDarkMode();
+    }
+    else {
+        setLightMode();
+    }
+}
+
+function setLightMode() {
+    root.style.setProperty('--background', '#FAFAFA');
+    root.style.setProperty('--text', 'black');
+    root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.1)')
+    setMode('white');
+    toggleSwitch.checked = false;
+}
+
+function setDarkMode() {
+    root.style.setProperty('--background', 'rgb(23,23,23)');
+    root.style.setProperty('--text', 'white');
+    root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.9)')
+    setMode('dark');
+    toggleSwitch.checked = true;
+}
 
 toggleSwitch.addEventListener("change", () => {
     if (!toggleSwitch.checked) {
-        root.style.setProperty('--background', '#FAFAFA');
-        root.style.setProperty('--text', 'black');
-        root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.1)')
+        setLightMode();
     }
     else {
-        root.style.setProperty('--background', 'rgb(23,23,23)');
-        root.style.setProperty('--text', 'white');
-        root.style.setProperty('--box-shadow', 'rgb(0,0,0, 0.9)')
+        setDarkMode();
     }
 });
+
+getMode();
