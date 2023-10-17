9e2631c352dad54c54f842ee30b1d768bd0e29a9
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-10-17 13:57:41 -0700

    Fixed comments model and foreign key relation

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 class Post(models.Model):
     """A blog post."""
+
     title = models.CharField(max_length=150)
     text = RichTextField()
     image = models.ImageField(upload_to='images/', blank=True)
@@ -19,6 +20,7 @@ class Post(models.Model):
 class Comment(models.Model):
     """A comment by a user."""
     
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=200)
 
